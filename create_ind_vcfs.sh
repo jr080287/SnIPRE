2e87bc58e8cdf5b4f9d8bcfb994a3e6d3a171ce7
tue39618 <tue39618@temple.edu>
2016-11-08 19:24:33 -0500

    fixed bug in create_ind_vcfs.sh

--- create_ind_vcfs.sh
+++ create_ind_vcfs.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 
-#create individual vcfs for all samples in a file
+#create the individual vcfs for all samples in a file
 
 # multi-ind vcf is 1st input argument
 
-for sample in `bcftools view -h $1 | grep "^#CHROM" | cut -f10-`; do
-    bcftools view -c1 -Oz -s $sample -o $.$sample.vcf.gz $1;
-done
+for sample in $(bcftools view -h  $1 | grep "^#CHROM" | cut -f10-); 
+    do
+        echo $sample
+        bcftools view  -Oz -s $sample $1 -o $sample.vcf.gz
+    done
